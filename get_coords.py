b736139a777c92d204d03100de6c66152a1ee3f7
Neal Paresh Patel <59032397+Desitrain22@users.noreply.github.com>
2024-03-29 16:27:51 -0400

    basic react app

--- get_coords.py
+++ get_coords.py
@@ -1,24 +1,24 @@
 import googlemaps
-from datetime import datetime
+import time
 
-# Initialize the Google Maps client with your API key
-gmaps = googlemaps.Client(key='AIzaSyBVmTDCW8zxGKB41JwUj8PIxxwRSxfjiPQ')
+def get_taco_bell_coords(api_key: str='AIzaSyBVmTDCW8zxGKB41JwUj8PIxxwRSxfjiPQ', location: tuple=(40.7831, -73.9712), radius: int=24140):
+    # Default is NYC, within 15 miles (reached out to Newark and the ass crack of Long Island or so)
+    gmaps = googlemaps.Client(key=api_key)
 
-# Define a location and radius (in meters)
-location = (40.7831, 73.9712)  # Example: Los Angeles, CA
-radius = 100  # Search within this radius
-# Perform a text search for "Taco Bell" locations within the radius
-results = gmaps.places(query="Taco Bell", location=location, radius=radius)
+    def fetch_places_nearby(client, location, radius, page_token=None):
+        # Perform a nearby search for "Taco Bell" locations within the radius
+        return client.places_nearby(location=location, radius=radius, keyword="Taco Bell", page_token=page_token)
 
-# Extract and print the names and locations
-for place in results['results']:
-    name = place['name']
-    lat = place['geometry']['location']['lat']
-    lng = place['geometry']['location']['lng']
-    print(f"{name} at Latitude: {lat}, Longitude: {lng}")
+    all_results = []
 
-coords = [(place['geometry']['location']['lat'], place['geometry']['location']['lng']) for place in results['results']]
+    results = fetch_places_nearby(gmaps, location, radius)
+    all_results.extend(results['results'])
 
-for i in coords:
-    print(i)
+    while 'next_page_token' in results:
+        time.sleep(2)  # Wait for the next_page_token to become valid
+        results = fetch_places_nearby(gmaps, location, radius, results['next_page_token'])
+        all_results.extend(results['results'])
 
+    return [(place['geometry']['location']['lat'], place['geometry']['location']['lng']) for place in all_results]
+
+get_taco_bell_coords(location=(39.5, -98.35), radius=100000)
