532bbc75368e6d83e2e25ed751f095cce5b792fc
Daniel Tao <danieltaox@gmail.com>
2024-03-30 14:50:12 -0400

    chore: kill constants. tailwind css variables is my new bff

--- mobile/constants.ts
+++
@@ -1,6 +0,0 @@
-export const PURPLE = '#662D91' as const;
-export const PINK = '#ED4B79' as const;
-export const MAGENTA = '#FF00FF' as const;
-export const GREEN = '#D9E021' as const;
-export const CYAN = '#D3FFFF' as const;
-export const LIGHT_GREEN = '#F0F3A6' as const;
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'purple': '#662D91',
+        'pink': '#ED4B79',
+        'magenta': '#FF00FF',
+        'green': '#D9E021',
+        'cyan': '#D3FFFF',
+        'light-green': '#F0F3A6',
+      },
+      fontFamily: {
+        stretch: ["Stretch Pro", "sans-serif"],
+        comic: ["Comic Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
