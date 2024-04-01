73abd3bd5d3c337fe6b469f941f6e74208148289
Daniel Tao <danieltaox@gmail.com>
2024-04-01 05:59:03 -0400

    feat: hit implemented history endpoints

--- mobile/app/authed/(tabs)/history.tsx
+++ mobile/app/authed/(tabs)/history.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import {
   Text as _Text,
   View as _View,
@@ -6,10 +7,11 @@ import {
 } from "react-native";
 import { styled } from "nativewind";
 import { useFonts } from "expo-font";
+import { router, usePathname } from 'expo-router';
 import { LinearGradient as _LinearGradient } from 'expo-linear-gradient';
 import PieChart from 'react-native-pie-chart'
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { colors, chartColors } from '../../../constants';
+import { BASE_URL, colors, chartColors } from '../../../constants';
 import Loading from '../../../components/Loading';
 
 const PIE_CHART_COLORS = Object.values(chartColors);
@@ -27,10 +29,38 @@ const generateRandomHexColor = () => {
 }
 
 export default function History() {
+  const [userId, setUserId] = useState('');
+  const [roomCode, setRoomCode] = useState('');
+  const [count, setCount] = useState<number>(0);
+  const [history, setHistory] = useState<any[]>([]);
+
   const [fontsLoaded] = useFonts({
     'Stretch Pro': require('../../../assets/fonts/Stretch Pro.otf'),
     'Comic Sans': require('../../../assets/fonts/Comic Sans.ttf')
   });
+
+  const path = usePathname();
+  useEffect(
+    () => {
+      (async () => {
+        const roomCode = await AsyncStorage.getItem('room_id');
+        if (!roomCode) return router.push('/onboarding');
+        setRoomCode(roomCode);
+
+        const userId = await AsyncStorage.getItem('user_id')
+        if (!userId) return router.push('/onboarding');
+        setUserId(userId);
+
+        const unloadableRes = await fetch(`${BASE_URL}/get_total_unloadable?room_id=${roomCode}&user_id=${userId}`);
+        const unloadableData = await unloadableRes.json();
+        setCount(unloadableData.dongs_unloadable);
+
+        const dongHistory = await fetch(`${BASE_URL}/get_dong_history?room_id=${roomCode}&user_id=${userId}`);
+        const dongHistoryData = await dongHistory.json();
+        setHistory(dongHistoryData.dongs);
+      })();
+    }
+    , [path])
 
   if (!fontsLoaded) return <Loading />;
 
@@ -54,7 +84,7 @@ export default function History() {
       >
         <View className="w-screen flex items-center justify-center pt-4">
           <Text className="font-stretch text-lg text-purple leading-5">You Have</Text>
-          <Text className="font-stretch text-7xl text-purple">12</Text>
+          <Text className="font-stretch text-7xl text-purple">{count}</Text>
           <Text className="font-stretch text-lg text-purple leading-5">DDongggs to Unload</Text>
         </View>
       </ImageBackground>
@@ -79,21 +109,15 @@ export default function History() {
 
       <View className="h-1/4">
         <ScrollView className="flex-col w-full px-2 space-y-2">
-          <View className="flex-row items-center justify-center border-solid border-purple border-2 w-full h-16 rounded-full px-8">
-            <Text className="font-comic text-xs text-purple mr-4">Monday</Text>
-            <Text className="font-stretch text-xs text-purple">Neal Patel</Text>
-            <Text className="font-stretch text-xs text-pink ml-auto">📍58A Fulton St.</Text>
-          </View>
-          <View className="flex-row items-center justify-center border-solid border-purple border-2 w-full h-16 rounded-full px-8">
-            <Text className="font-comic text-xs text-purple mr-4">Tuesday</Text>
-            <Text className="font-stretch text-xs text-purple">Neal Patel</Text>
-            <Text className="font-stretch text-xs text-pink ml-auto">📍58A Fulton St.</Text>
-          </View>
-          <View className="flex-row items-center justify-center border-solid border-purple border-2 w-full h-16 rounded-full px-8">
-            <Text className="font-comic text-xs text-purple mr-4">Sunday</Text>
-            <Text className="font-stretch text-xs text-purple">Neal Patel</Text>
-            <Text className="font-stretch text-xs text-pink ml-auto">📍58A Fulton St.</Text>
-          </View>
+          {history.map(({
+            dongee,
+            dong_time,
+            location
+          }) => <View className="flex-row items-center justify-center border-solid border-purple border-2 w-full h-16 rounded-full px-8">
+            <Text className="font-comic text-xs text-purple mr-4">{dong_time}</Text>
+            <Text className="font-stretch text-xs text-purple">{dongee}</Text>
+            <Text className="font-stretch text-xs text-pink ml-auto">📍{location}</Text>
+          </View>)}
         </ScrollView>
       </View>
     </View>
